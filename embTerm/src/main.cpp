b499645f5ad6df301c67414aa46d59c1ef08929e
germanoavs <germanoavs@gmail.com>
2020-08-15 11:20:04 -0300

    Update main.cpp

--- embTerm/src/main.cpp
+++ embTerm/src/main.cpp
@@ -1,27 +1,32 @@
 #include<Arduino.h>
 #include <Arduino_FreeRTOS.h>
-#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
 #include<DallasTemperature.h>
 #include<OneWire.h>
+#include <EEPROM.h>
+#include <queue.h>
+#include <stdio.h>
 
 
 // define two Tasks for DigitalRead & AnalogRead
-void Task0( void *pvParameters );
-void Task1( void *pvParameters );
-void Task2( void *pvParameters );
-
-// semaphore
-SemaphoreHandle_t xBufferSemaphore;
+void sensorTask( void *pvParameters );
+void terminalTask( void *pvParameters );
+void eepromTask( void *pvParameters );
+void buttonInterrupt();
+float CalcMedia();
+void clearEEPROM();
+char *ftoa(char *a, double f, int precision);
 
 #define DS18B20 7 //DEFINE O PINO DIGITAL UTILIZADO PELO SENSOR
-#define BUFFER_SIZE 10
-
-uint8_t BUFFER[BUFFER_SIZE];
-uint8_t bfindex = 0;
-uint8_t media;
+#define MEMORY_SIZE 1024/sizeof(float)
+#define MEMORY_FLAG ((float)0xffff)
+const byte interruptPin = 2;
+int memory_index = -1*sizeof(float);
+bool FULL_MEMORY = false;
 
 OneWire ourWire(DS18B20);
 DallasTemperature sensors(&ourWire);
+QueueHandle_t queue_1;
+QueueHandle_t queue_2;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -30,33 +35,42 @@ void setup() {
   Serial.begin(9600);
   sensors.begin();
 
-    if ( xBufferSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
-  {
-    xBufferSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Buffer
-    if ( ( xBufferSemaphore ) != NULL )
-      xSemaphoreGive( ( xBufferSemaphore ) );  // Make the Buffer available for use, by "Giving" the Semaphore.
+  // clear EEPROM, set all bits to 0;
+  clearEEPROM();
+
+  // set interrupt pin
+  pinMode(interruptPin, INPUT_PULLUP);
+
+  // set interrupt
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonInterrupt, CHANGE);
+
+  // create queue's
+  queue_1 = xQueueCreate(6, sizeof(char *));
+  queue_2 = xQueueCreate(3, sizeof(float));
+
+  if (queue_1 == NULL || queue_2 == NULL) {
+    Serial.println("Queue can not be created");
   }
-  
 
   // Now set up two Tasks to run independently.
   xTaskCreate(
-    Task0
-    ,  "ReadSensor"  // A name just for humans
+    sensorTask
+    ,  "LeSensor"  // A name just for humans
     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
-    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL );
 
   xTaskCreate(
-    Task1
-    ,  "CalMedia"
+    terminalTask
+    ,  "CalcMedia"
     ,  128  // Stack size
     ,  NULL
-    ,  2  // Priority
+    ,  3  // Priority
     ,  NULL );
 
   xTaskCreate(
-    Task2
+    eepromTask
     ,  "PrintMedia"
     ,  128  // Stack size
     ,  NULL
@@ -73,67 +87,108 @@ void loop()
   // Empty. Things are done in Tasks.
 }
 
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
 
-void Task0( void *pvParameters __attribute__((unused)) )  // This is a Task.
+/////////////////////////  SENSOR TASK ///////////////////////////////////////////////////////
+void sensorTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+    float current_temperature;
 
-
-  for (;;) 
+   while(1) 
   {
-    if ( xSemaphoreTake( xBufferSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-    {
-      while (bfindex<BUFFER_SIZE)
-      {
-        BUFFER[bfindex] = sensors.getTempCByIndex(0);
-        bfindex++;
-      }
-      xSemaphoreGive( xBufferSemaphore ); // Now free or "Give" the Serial Port for others.
-    }
+    sensors.requestTemperatures();            //SOLICITA QUE A FUNÇÃO INFORME A TEMPERATURA DO SENSOR
+    current_temperature = sensors.getTempCByIndex(0);
+    String mensagem = "Temperatura Sensor: " + (String)current_temperature;
 
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability 
+    xQueueSend(queue_1, &mensagem, portMAX_DELAY);
+    xQueueSend(queue_2, &current_temperature, portMAX_DELAY);
+
+    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability 
   }
 }
 
-void Task1( void *pvParameters __attribute__((unused)) )  // This is a Task.
+/////////////////////////  TERMINAL TASK ///////////////////////////////////////////////////////
+void terminalTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+   char *mensagem;
 
-  for (;;)
-  {
-    if ( xSemaphoreTake( xBufferSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-    { 
-      uint32_t sum = 0;
-      uint8_t i = BUFFER_SIZE-1;
-
-      while (i>=0)
-      {
-        sum+= BUFFER[i];
-        i--;
-      }
-      
-      media = sum/BUFFER_SIZE;
-      bfindex=0;
-
-      xSemaphoreGive( xBufferSemaphore ); // Now free or "Give" the Serial Port for others.
+  while(1) {
+    if (xQueueReceive(queue_1, &mensagem, portMAX_DELAY) == pdPASS) {
+      Serial.print(mensagem);
+      Serial.print("\n--------------------------\n");
     }
-
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
-
+    
   }
+
 }
 
-void Task2( void *pvParameters __attribute__((unused)) )  // This is a Task.
+/////////////////////////  EEPROM TASK ///////////////////////////////////////////////////////
+void eepromTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+    float temperature = 0.0;
 
-  for (;;)
-  {
-    Serial.print("Media Temperatura: "); //IMPRIME O TEXTO NA SERIAL
-    Serial.print(media); //IMPRIME NA SERIAL O VALOR DE TEMPERATURA MEDIDO
-    Serial.println("*C"); //IMPRIME O TEXTO NA SERIAL
+   while(1) {
+      if (xQueueReceive(queue_2, &temperature, portMAX_DELAY) == pdPASS) {
+        memory_index += sizeof(float);
+        EEPROM.put(memory_index, temperature);
 
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+        if(memory_index > MEMORY_SIZE){
+           memory_index = -4;
+           FULL_MEMORY = true;
+        }
+     }
+    }
+  
+}
 
+//////////////////////////////// INTERRUPT TASK MEDIA ///////////////////////////////////////
+float CalcMedia(){
+
+  int i = memory_index;
+  float sum = 0;
+  float temp;
+
+  if(FULL_MEMORY)
+    i = MEMORY_SIZE;
+  
+  for (; i >= 0;)
+  { 
+    EEPROM.get(i, temp);
+    
+    sum += temp;  
+     i -= (sizeof(float));
+  }
+  
+
+  if (FULL_MEMORY)
+    return sum/MEMORY_SIZE;
+  else{
+    return sum/((memory_index/sizeof(float))+1);
+  }
+
+}
+
+void buttonInterrupt(){
+
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200){
+  float media = CalcMedia();
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+  String mensagem = "Média: " + (String)media;
+  //Serial.print(mensagem);
+  xQueueSendFromISR( queue_1, &mensagem, &xHigherPriorityTaskWoken);
+
+  portYIELD_FROM_ISR();
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+// EEPROM CLEAR
+
+void clearEEPROM(){
+  for (uint16_t i = 0 ; i <= MEMORY_SIZE ; i++) {
+    EEPROM.put(i, 0);
   }
 }
