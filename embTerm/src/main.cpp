1e706e9a6944a61cf66e57bd23e07eadfb2b4c2a
germanoavs <germanoavs@gmail.com>
2020-08-15 15:27:30 -0300

    Add Doxygen Documentaion

--- embTerm/src/main.cpp
+++ embTerm/src/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @mainpage Embedded Thermometer
+ * @author Germano Sobroza
+ * 1. This project works with the use of an Arduino Uno board, temperature sensor with a serial module and a push-button.
+ * 2. The temperature sensor is read in real time and the measurements are sent to the terminal and memory tasks.
+ * 3. If the push-button is pressed, an interruption is triggered and the averaged of the measures stored in memory are calculated 
+ *  and sent to the terminal task.
+ */
 #include<Arduino.h>
 #include <Arduino_FreeRTOS.h>
 #include<DallasTemperature.h>
@@ -16,19 +24,23 @@ float CalcMedia();
 void clearEEPROM();
 char *ftoa(char *a, double f, int precision);
 
-#define DS18B20 7 //DEFINE O PINO DIGITAL UTILIZADO PELO SENSOR
-#define MEMORY_SIZE 1024/sizeof(float)
-#define MEMORY_FLAG ((float)0xffff)
-const byte interruptPin = 2;
-int memory_index = -1*sizeof(float);
-bool FULL_MEMORY = false;
+#define DS18B20 7                             ///< Define the digital pin used by the sensor
+#define MEMORY_SIZE 1024/sizeof(float)        ///< Memory lenght
+
+const byte interruptPin = 2;                  ///< Interrup pin used by the button
+int memory_index = -1*sizeof(float);          ///< Memory index variable, initialized with -1*sizeof(float) for implentation purposes 
+bool FULL_MEMORY = false;                     ///< when all positions have been written with some temperature, turn into true
 
 OneWire ourWire(DS18B20);
 DallasTemperature sensors(&ourWire);
 QueueHandle_t queue_1;
 QueueHandle_t queue_2;
 
-// the setup function runs once when you press reset or power the board
+/**
+ * @brief The setup() function intialize the serial monitor, the DallasTemperature sensor, configure the interrupt pin,
+ * clears the EEPROM memory, creates two queues and three tasks.
+ * 
+*/
 void setup() {
 
   // initialize serial communication at 9600 bits per second:
@@ -52,7 +64,7 @@ void setup() {
     Serial.println("Queue can not be created");
   }
 
-  // Now set up two Tasks to run independently.
+  // Set up Tasks to run independently.
   xTaskCreate(
     sensorTask
     ,  "LeSensor"  // A name just for humans
@@ -63,7 +75,7 @@ void setup() {
 
   xTaskCreate(
     terminalTask
-    ,  "CalcMedia"
+    ,  "Terminal"
     ,  128  // Stack size
     ,  NULL
     ,  3  // Priority
@@ -71,7 +83,7 @@ void setup() {
 
   xTaskCreate(
     eepromTask
-    ,  "PrintMedia"
+    ,  "Memória"
     ,  128  // Stack size
     ,  NULL
     ,  1  // Priority
@@ -88,25 +100,33 @@ void loop()
 }
 
 
-/////////////////////////  SENSOR TASK ///////////////////////////////////////////////////////
+/**
+* @brief sensorTask() reads the temperature from the sensor, convert it to a String 
+* type and send it througth queue_1 to terminalTask(). After that, queue_2 sends the float
+* value of the temperature to eepromTask. 
+*
+*/
 void sensorTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
-    float current_temperature;
+    float current_temperature;                ///< stores the read temperature
 
    while(1) 
   {
-    sensors.requestTemperatures();            //SOLICITA QUE A FUNÇÃO INFORME A TEMPERATURA DO SENSOR
-    current_temperature = sensors.getTempCByIndex(0);
-    String mensagem = "Temperatura Sensor: " + (String)current_temperature;
+    sensors.requestTemperatures();                          
+    current_temperature = sensors.getTempCByIndex(0);               // get the last temperature
+    String mensagem = "Temperatura Sensor: " + (String)current_temperature;         // cast float to String
 
-    xQueueSend(queue_1, &mensagem, portMAX_DELAY);
-    xQueueSend(queue_2, &current_temperature, portMAX_DELAY);
+    xQueueSend(queue_1, &mensagem, portMAX_DELAY);                  // sends a char pointer throught queue_1
+    xQueueSend(queue_2, &current_temperature, portMAX_DELAY);       // sends a float throught queue_2
 
     vTaskDelay(100);  // one tick delay (15ms) in between reads for stability 
   }
 }
 
-/////////////////////////  TERMINAL TASK ///////////////////////////////////////////////////////
+/**
+* @brief terminalTask() waits for messages sent thought queue_2 and print
+* it in the terminal.
+*/
 void terminalTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
    char *mensagem;
@@ -121,7 +141,12 @@ void terminalTask( void *pvParameters __attribute__((unused)) )  // This is a Ta
 
 }
 
-/////////////////////////  EEPROM TASK ///////////////////////////////////////////////////////
+
+/**
+* @brief eepromTask() reiceves float temperature and writes in the EEPROM, addressed
+* by memory_index. If memory_index is bigger than MEMORY_SIZE, memory_indez is set to
+* -sizeof(float), working like a circular buffer.
+*/
 void eepromTask( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
     float temperature = 0.0;
@@ -129,18 +154,22 @@ void eepromTask( void *pvParameters __attribute__((unused)) )  // This is a Task
    while(1) {
       if (xQueueReceive(queue_2, &temperature, portMAX_DELAY) == pdPASS) {
         memory_index += sizeof(float);
-        EEPROM.put(memory_index, temperature);
 
         if(memory_index > MEMORY_SIZE){
            memory_index = -4;
            FULL_MEMORY = true;
         }
+
+        EEPROM.put(memory_index, temperature);            // writes temperature in memory_index address
      }
     }
   
 }
 
-//////////////////////////////// INTERRUPT TASK MEDIA ///////////////////////////////////////
+/**
+* @brief CalMedia() is called by the buttonInterrupt() and calculates the mean
+* of all temperatures wroten on the EEPROM
+*/
 float CalcMedia(){
 
   int i = memory_index;
@@ -167,26 +196,32 @@ float CalcMedia(){
 
 }
 
+/**
+* @brief buttonInterrupt() deals with an interrupt generated by an external button. The ISR calls
+* CalMedia function and send the mean the temperature throught queue_2
+*/
 void buttonInterrupt(){
 
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
+
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time > 200){
-  float media = CalcMedia();
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    float media = CalcMedia();
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-  String mensagem = "Média: " + (String)media;
-  //Serial.print(mensagem);
-  xQueueSendFromISR( queue_1, &mensagem, &xHigherPriorityTaskWoken);
+    String mensagem = "Média: " + (String)media;
+    xQueueSendFromISR( queue_1, &mensagem, &xHigherPriorityTaskWoken);
 
-  portYIELD_FROM_ISR();
+    // request a context switch from ISR
+    portYIELD_FROM_ISR();
   }
   last_interrupt_time = interrupt_time;
 }
 
-// EEPROM CLEAR
-
+/**
+* @brief clearEEPROM() set all EEPROM bits to 0
+*/
 void clearEEPROM(){
   for (uint16_t i = 0 ; i <= MEMORY_SIZE ; i++) {
     EEPROM.put(i, 0);
